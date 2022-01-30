3229fed49a4afa0e89a818213e9021d27ed2db64
SANDEEP BURDAGUNTA <Sandeep@sandeeps-mbp.lan>
2022-01-30 16:26:54 -0700

    test code

---
+++ src/main/java/org/tista/HomeController.java
@@ -0,0 +1,12 @@
+package org.tista;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String viewHome() {
+        return "index";
+    }
+} 
